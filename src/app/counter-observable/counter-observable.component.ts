cf55946930a8b33203a468179249d143d24b6f69
jbernal <j.bernal@upm.es>
2017-12-19 19:03:57 +0100

    update counter

--- src/app/counter-observable/counter-observable.component.ts
+++ src/app/counter-observable/counter-observable.component.ts
@@ -4,7 +4,7 @@ import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
 
 @Component({
-    selector: 'app-counter',
+    selector: 'app-counter-observable',
     template: `<h3>Counter</h3>
                <button (click)="increase()">+</button> {{counter}}
                <h3>Observer: {{observer}}</h3>`
