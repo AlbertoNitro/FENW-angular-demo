93c4c4c3aef0a7a47066742fecbb9bdd6e04d64c
jbernal <j.bernal@upm.es>
2017-12-21 13:48:54 +0100

    update

--- src/app/core/http.service.ts
+++ src/app/core/http.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class HttpService {
